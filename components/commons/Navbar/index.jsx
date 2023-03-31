96efe97e1e8f262641748b4c3c149287491764ad
Ajinkya Kahane <kahaneajinkya051@gmail.com>
2023-04-01 05:09:48 +0530

    Home Page Completed With Mobile Responsiveness

--- components/commons/Navbar/index.jsx
+++ components/commons/Navbar/index.jsx
@@ -1,15 +1,221 @@
-import { Person, Search, ShoppingCart } from '@mui/icons-material'
-import { Box, AppBar, Container, Toolbar, Typography, Button, IconButton, Avatar } from '@mui/material'
-import React from 'react'
+import { Close, MenuBook, Person, Search, ShoppingCart } from '@mui/icons-material'
+import { Box, AppBar, Container, Toolbar, Typography, Button, IconButton, Avatar, Menu, Drawer, InputBase, Divider } from '@mui/material'
+import React, { useState } from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+import Link from 'next/link';
+
+const SubDrawer = ({open, setOpen}) => {
+    return (
+        <Box
+            px={2}
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '2vh',
+                mb: 10
+            }}
+        >
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    width: '100%'
+                }}
+            >
+                <Typography
+                    sx={{
+                        fontFamily: 'Rapier',
+                        fontSize: '35px',
+                        // color: '#85111D'
+                        color: 'black'
+                    }}
+                >
+                    ROGUE<span style={{ color: '#85111D' }}>.</span>
+                </Typography>
+                <IconButton
+                    onClick={() => setOpen(!open)}
+                >
+                    <Close
+                        sx={{
+                            color: 'rgba(0, 0, 0, 1)'
+                        }}
+                    />
+                </IconButton>
+            </Box>
+
+            <Divider
+                sx={{
+                    width: ['50vw', '10vw'],
+                    marginX: 'auto'
+                    // backgroundColor: 'rgba(0, 0, 0, 0.4)'
+                }}
+            />
+
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    gap: '2vh',
+                }}
+            >
+                <Link
+                    href={'/'}
+                    style={{ textDecorationLine: 'none' }}
+                >
+                    <Typography
+                        sx={{
+                            color: 'black',
+                            fontFamily: 'Montserrat'
+                        }}
+                    >
+                        Men&apos;s
+                    </Typography>
+                </Link>
+                <Link
+                    href={'/'}
+                    style={{ textDecorationLine: 'none' }}
+                >
+                    <Typography
+                        sx={{
+                            color: 'black',
+                            fontFamily: 'Montserrat'
+                        }}
+                    >
+                        Women&apos;s
+                    </Typography>
+                </Link>
+                <Link
+                    href={'/'}
+                    style={{ textDecorationLine: 'none' }}
+                >
+                    <Typography
+                        sx={{
+                            color: 'black',
+                            fontFamily: 'Montserrat'
+                        }}
+                    >
+                        Beauty
+                    </Typography>
+                </Link>
+            </Box>
+            <Divider
+                sx={{
+                    mt: '1vh',
+                    mb: '3vh',
+                    width: ['50vw', '10vw'],
+                    marginX: 'auto'
+                    // backgroundColor: 'rgba(0, 0, 0, 0.4)'
+                }}
+            />
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    gap: '5vw'
+                }}
+            >
+                <Button
+                    sx={{
+                        color: 'black',
+                        display: 'flex',
+                        gap: '1vw',
+                        borderRadius: '30px',
+                        px: 3,
+                        py: 0.5
+                    }}
+                >
+                    <ShoppingCart
+                    
+                    />
+                    <Typography
+                        sx={{
+                            color: 'black'
+                        }}
+                    >
+                        Cart
+                    </Typography>
+                </Button>
+                <Button
+                    sx={{
+                        color: 'black',
+                        display: 'flex',
+                        gap: '1vw',
+                        borderRadius: '30px',
+                        px: 3,
+                        py: 0.5
+                    }}
+                >
+                    <Person />
+                    <Typography>
+                        Profile
+                    </Typography>
+                </Button>
+            </Box>
+
+            <Divider
+                sx={{
+                    mt: '1vh',
+                    mb: '3vh',
+                    width: ['50vw', '10vw'],
+                    marginX: 'auto'
+                    // backgroundColor: 'rgba(0, 0, 0, 0.4)'
+                }}
+            />
+
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    flexDirection: 'column',
+                    gap: '4vh'
+                }}
+            >
+                <InputBase
+                    sx={{
+                        border: '1px solid grey',
+                        width: '50vw',
+                        borderRadius: '20px',
+                        px: '6px',
+                        py: '2px'
+                    }}
+                    placeholder='Search'
+                    startAdornment={<Search sx={{ ml: 1.5 }} />}
+                />
+                <Button
+                    sx={{
+                        bgcolor: 'black',
+                        border: '1px solid black',
+                        borderRadius: '20px',
+                        color: 'white',
+                        px: 3,
+                        ":hover": {
+                            backgroundColor: 'white',
+                            color: 'black'
+                        }
+                    }}
+                >
+                    Search
+                </Button>
+            </Box>
+        </Box>
+    )
+}
 
 const Index = () => {
+
+    const [open, setOpen] = useState(false);
+
     return (
         <AppBar
             sx={{
                 backgroundColor: '#F7F7F7',
                 boxShadow: 'none',
                 pl: 2.5,
-                pr: 2.5,
+                pr: 3,
                 mx: 0
             }}
         >
@@ -44,7 +250,7 @@ const Index = () => {
 
                     <Box
                         sx={{
-                            display: 'flex',
+                            display: ['none', 'none', 'flex'],
                             gap: '2vw'
                         }}
                     >
@@ -109,7 +315,7 @@ const Index = () => {
 
                     <Box
                         sx={{
-                            display: 'flex',
+                            display: ['none', 'flex', 'flex'],
                             gap: '1vw'
                         }}
                     >
@@ -148,6 +354,39 @@ const Index = () => {
                                 }}
                             />
                         </IconButton>
+                        <IconButton
+                            sx={{
+                                display: ['flex', 'flex', 'none'],
+                                ml: 5
+                            }}
+                            onClick={() => setOpen(!open)}
+                        >
+                            {open &&
+                                <Close
+                                    sx={{
+                                        color: 'rgba(0, 0, 0, 1)'
+                                    }}
+                                />
+                            }
+                            {!open &&
+                                <MenuIcon
+                                    sx={{
+                                        color: 'rgba(0, 0, 0, 1)'
+                                    }}
+                                />
+                            }
+                        </IconButton>
+
+                        <Drawer
+                            anchor={'top'}
+                            open={open}
+                            onClose={() => setOpen(!open)}
+                            sx={{
+                                mt: 8
+                            }}
+                        >
+                            <SubDrawer open={open} setOpen={setOpen} />
+                        </Drawer>
                     </Box>
                 </Toolbar>
             </Container>
