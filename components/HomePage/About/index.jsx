c794d9992368785d4cb2017458c663cf5fbe5d1f
Ajinkya Kahane <kahaneajinkya051@gmail.com>
2023-03-31 05:37:29 +0530

    About Section Updated

--- components/HomePage/About/index.jsx
+++ components/HomePage/About/index.jsx
@@ -1,44 +1,62 @@
+import { FormatQuote } from '@mui/icons-material'
 import { Box, Typography } from '@mui/material'
 import React from 'react'
 
 const Index = () => {
-  return (
-    <Box
-      px={5}
-    >
-      <Box
-        sx={{
-          display: 'flex',
-        }}
-      >
+    return (
         <Box
+            px={5}
         >
-          <Typography
-            // width={'80vw'}
-            sx={{
-              width: '55vw',
-              fontFamily: 'Futura',
-              fontSize: '200px',
-              color: 'rgba(0, 0, 0, 0.2)',
-            }}
-          >
-            About Us
-          </Typography>
+            <Box
+                sx={{
+                    display: 'flex',
+                }}
+            >
+                <Box
+                >
+                    <Typography
+                        // width={'80vw'}
+                        sx={{
+                            width: '55vw',
+                            fontFamily: 'Futura',
+                            fontSize: '200px',
+                            color: 'rgba(0, 0, 0, 0.2)',
+                        }}
+                    >
+                        About Us
+                    </Typography>
+                </Box>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        position: 'relative'
+                    }}
+                >
+                    <FormatQuote 
+                        sx={{
+                            position: 'absolute',
+                            top: '1%',
+                            left: '-8%',
+                            transform: 'rotate(180deg)',
+                            fontSize: '100px',
+                            color: 'rgba(0, 0, 0, 0.2)'
+                        }}
+                    />
+                    <Typography
+                        sx={{
+                            fontSize: '18px',
+                            textAlign: 'center',
+                            fontFamily: 'Montserrat'
+                        }}
+                    >
+                        Welcome to <span style={{color: '#85111B', fontWeight: 600}}>Rogue Fashions </span>We offer trendy and affordable clothing for Gen Z that empowers self-expression. Our mission is to make a positive impact through sustainability and inclusivity. Join us in embracing your unique style!
+                    </Typography>
+                </Box>
+            </Box>
         </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}
-        >
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </Typography>
-        </Box>
-      </Box>
-    </Box>
-  )
+    )
 }
 
 export default Index
